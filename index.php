2f7704df8e7e5c478c1f0f65d15477c1f8347f9a
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2020-11-16 14:37:13 +0100

    structure du site

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="container">
+        <div class="page-header" id="banner">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Let's cook</h1>
+                    <p class="lead">Partageons nos recettes</p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+</html>
