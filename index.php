e621c0a088e368b71c2d6a404e6490d4c584c26b
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2020-11-16 17:11:17 +0100

    index.php en construction

--- index.php
+++ index.php
@@ -4,20 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
     <title>Accueil</title>
 </head>
 <body>
     <div class="container">
-        <div class="page-header" id="banner">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Let's cook</h1>
-                    <p class="lead">Partageons nos recettes</p>
+        <div class="page-header" >
+        
+            <div id="banner">
+                <div class="row">
+                    <div class="col-lg-12">
+                    
+                        <h1>Let's cook</h1>
+                        <p class="lead">Partageons nos recettes</p>
+                    </div>
+                    
                 </div>
             </div>
+            
         </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home
+                    <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="connexion.php">Connexion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="inscription.php">Inscription</a>
+                </li>
+                </ul>  
+            </div>
+        </nav>
+        <main class="jumbotron">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    <img src="images/bol.jpg" alt="bol">
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <p>Pour accéder à notre espace membre, veuillez-vous connecter.</p><br/>
+                    <button type="button" class="btn btn-success btn-lg">Connexion</button>
+                </div>
+            </div>
+        </main>
     </div>
 
 </body>
