fb73fdc4da30eba748b80e797da8ef289c49400a
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2020-11-17 13:52:28 +0100

    connexion modifié

--- connexion.php
+++ connexion.php
@@ -50,6 +50,7 @@
             <div class="row">
                 
                 <div class="col-lg-6 col-sm-12"><br/>
+                <h2>Espace membres</h2>
                 <form action="profil.php" method="post">
                     <fieldset>   
                         <div class="form-group">
@@ -71,22 +72,49 @@
                 <div class="col-lg-6 col-sm-12">
                     <img class="img-small" src="assets/images/plat.jpg" alt="plat">
                 </div>
+
             </div>
         </main>
+        
+            <article class="jumbotron">  
+                <h2>Administration</h2> 
+                <form action="admin.php" method="post">
+                    
+                    <fieldset>
+                        <div class="row">    
+                            <div class="form-group col-lg-4 col-sm-12">
+                            <label for="login">Login</label>
+                            <input type="txt" class="form-control" id="login"  name="login" placeholder="Entrer Login">
+                            </div>
+                                                
+                            <div class="form-group col-lg-4 col-sm-12">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            </div>
+                            <div class="form-group col-lg-12 col-sm-12">
+                            
+                            <button type="submit" class="btn btn-info">Connexion</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </article>        
+                             
+        
         <footer id="footer">
-        <div class="row">
-          <div class="col-lg-12">
-            <ul class="list-unstyled">
-              <li class="float-lg-right"><a href="#top">Back to top</a></li>
-              
-              <li><a href="https://github.com/thomaspark/bootswatch">GitHub</a></li>
-              
-            </ul>
-            <p>Made by <a href="https://thomaspark.co/">Thomas Park</a>.</p>
-            <p>Code released under the <a href="https://github.com/thomaspark/bootswatch/blob/master/LICENSE">MIT License</a>.</p>
-            
-          </div>
-        </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-unstyled">
+                    <li class="float-lg-right"><a href="#top">Back to top</a></li>
+                    
+                    <li><a href="https://github.com/thomaspark/bootswatch">GitHub</a></li>
+                    
+                    </ul>
+                    <p>Made by <a href="https://thomaspark.co/">Thomas Park</a>.</p>
+                    <p>Code released under the <a href="https://github.com/thomaspark/bootswatch/blob/master/LICENSE">MIT License</a>.</p>
+                    
+                </div>
+            </div>
       </footer>
     </div>
     <script src="assets/js/jquery.min.js"></script>
