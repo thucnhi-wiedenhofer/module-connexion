52db54e58e5ea0a254974f4cd3e1e0e5c36f4f39
denis farkas <denis.farkas@laplateforme.io>
2020-11-19 14:27:48 +0100

    modif bouton admin

--- admin.php
+++ admin.php
@@ -90,7 +90,10 @@ session_start();
                             echo "<td>".$resultats['prenom']."</td>";
                             echo "<td>".$resultats['nom']."</td>";
                             echo "<td>".$resultats['password']."</td>";
+                            if($resultats['login']!="admin"){
                             echo '<td><form action="profil.php" method="post"><button type="submit" class="btn btn-info btn-sm" value="'.$resultats['login'].'" name="modif_adm">Modifier</button></form></td>';
+                            }
+                            
                         echo "</tr>";
                         }
                     ?>  
