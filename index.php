643e41919710a72969441d063e68b3cd4df7e787
denis farkas <denis.farkas@laplateforme.io>
2020-11-19 14:20:47 +0100

    index admin

--- index.php
+++ index.php
@@ -80,7 +80,10 @@ if(isset($_POST['session_fin']))
                     echo '<p class="h4"> Bonjour '.$_SESSION['prenom'].' '.$_SESSION['nom'].'.</p><br />';
                     echo '<p class="h5">Pour vérifier ou modifier vos informations:</p>';
                     echo '<form action="profil.php" method="post"><button type="submit" class="btn btn-primary btn-lg btn-block" name="modifier">Consulter</button></form>';
-                }else{
+                }elseif($_SESSION['nom']=="admin"){
+                    echo '<p class="h4"> Bonjour '.$_SESSION['prenom'].'.</p><br />';
+                }
+                else{
                     echo '<p class="h5">Vous n\'avez pas d\'idée pour ce soir? <br/>Consultez et échangez vos recettes entre membres.<br/>
                      Pour accéder à notre espace abonné, veuillez-vous connecter.</p><br/>
                     <a href="connexion.php"><button type="button" class="btn btn-success btn-lg">Connexion</button><br/><br/></a>
